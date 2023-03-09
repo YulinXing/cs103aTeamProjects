a08d3bac389fe3e701f992af9b2f7891b1e57bed
YulinXing <18211193935@163.com>
2023-03-09 13:12:30 -0500

    added method

--- ca01/gpt.py
+++ ca01/gpt.py
@@ -18,20 +18,27 @@ On Windows:
 % python gpt.py
 '''
 import openai
+import os
+import base64
+import requests
+from PIL import Image
+from io import BytesIO
+
 
 
 class GPT():
     ''' make queries to gpt from a given API '''
-    def __init__(self,apikey):
+    def __init__(self, apikey):
         ''' store the apikey in an instance variable '''
-        self.apikey=apikey
+        self.apikey = apikey
         # Set up the OpenAI API client
         openai.api_key = apikey #os.environ.get('APIKEY')
 
         # Set up the model and prompt
-        self.model_engine = "text-davinci-003"
+        self.model_engine = "davinci"
+        self.image_model = "image-alpha-001"
 
-    def getResponse(self,prompt):
+    def getResponse(self, prompt):
         ''' Generate a GPT response '''
         completion = openai.Completion.create(
             engine=self.model_engine,
@@ -45,13 +52,36 @@ class GPT():
         response = completion.choices[0].text
         return response
 
-if __name__=='__main__':
+    def generateImage(self, prompt):
+        ''' Generate an image from the prompt '''
+        result = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size="512x512",
+            model=self.image_model,
+            response_format="url"
+        )
+
+        img = Image.open(BytesIO(requests.get(result['data'][0]['url']).content))
+        buffered = BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+
+        return img_str
+    
+    # add your methods below
+
+
+if __name__ == '__main__':
     '''
     '''
-    import os
     g = GPT(os.environ.get("APIKEY"))
-<<<<<<< HEAD
-    print(g.getResponse("what does openai's GPT stand for?"))
-=======
-    print(g.getResponse("what does openai's GPT stand for?"))
->>>>>>> 10dba5c6347d93ff318ead8b9d50ef6810aa822a
+
+    # # Generate some data for the plot
+    # x = np.linspace(0, 10, 100)
+    # y = np.sin(x)
+
+    # # Generate the plot
+    # g.generatePlot(x, y, xlabel="x", ylabel="y", title="Plot of y = sin(x)")
+
+    g.generateImage("a cat in an apple")
---
+++ ca01/key.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+export APIKEY="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"
+$1
