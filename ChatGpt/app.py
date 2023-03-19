f441721bbff4a3b91b4866f50c65622e3ae0e4c7
yishan122 <122732698+yishan122@users.noreply.github.com>
2023-03-19 01:53:27 -0400

    Update app.py

--- ChatGpt/app.py
+++ ChatGpt/app.py
@@ -25,16 +25,16 @@ from chatgpt import GPT
 import os
 
 app = Flask(__name__)
-# 设置运行环境未开发环境
+# 设
 app.config.from_object(settings.DevelopmentConfig)
-# 调用GPT的类，实例化一个gptAPI的对象
+# 
 gptAPI = GPT(os.environ.get('APIKEY'))
 
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'xamdjqowid%Y@*IKOJ@QHdoabdiuawn3w5'
 
 
-# 首页
+# frontpage
 @app.route('/')
 def index():
     ''' display a link to the general query page '''
@@ -44,13 +44,13 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # 如果请求方法为POST则执行下列代码
+    # run if post
     if request.method == 'POST':
-        # 获取账号
+        # acess acount
         name = request.form.get('username')
-        # 获取密码
+        # acess password
         password = request.form.get('password')
-        # 判断如果账号为张三，密码为123，（这里我把账号密码写死，也可以和数据库连接变成动态）则跳转到访问中心页面
+        # Determine account password
         if name == '张三' and password == '123':
             return render_template('center.html')
         else:
@@ -63,17 +63,13 @@ def gptdemo():
         and a post request by returning the GPT response
     '''
     if request.method == 'POST':
-        # 获取输入框里面的问题内容
+        # acess the question
         question = request.form['question']
-        # 通过调用GPT的方法获取返回内容，但因为需要GPT账号，故无法实现
-        # answer = gptAPI.getResponse(prompt)
-        answer = '对不起，您输入的内容有误，请重新输入！！！'
-        # 渲染模板，并把参数传递至模板中
+        answer = 'Sorry, reenter again'
         return render_template('response.html', question=question, answer=answer)
     else:
         return render_template('center.html')
 
 
 if __name__ == '__main__':
-    # 在500这个端口启动项目
     app.run(debug=True, port=5000)
